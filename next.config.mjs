fde905844647c5d9696517127d4f7d4b5ce4eba1
surgvimon <surgvimon@gmail.com>
2024-02-29 10:52:09 +0700

    build docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    output: 'export',
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
+    trailingSlash: true,
     
     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
     // skipTrailingSlashRedirect: true,
     
     // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'docs',
+    distDir: 'docs',
 
     webpack(config) {
         config.module.rules.push({
